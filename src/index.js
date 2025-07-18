8e6bbbd848bda4b8953a255bb072fe0d2ad1c597
Andrei Tuicu <tuicu@adobe.com>
2025-07-18 17:23:50 +0200

    Protected Content (without assets) - Restrict to Helix Admin IP

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import getObject from './storage/object';
 import { get404, daResp, getRobots } from './responses/index';
 import getFromAdmin from './storage/admin';
 
+// https://www.aem.live/docs/security#backends-with-ip-filtering
+const HELIX_ADMIN_IP = '3.227.118.73';
 const EMBEDDABLE_ASSETS_EXTENSIONS = [ '.jpg', '.jpeg', '.png', '.svg', '.pdf', '.gif', '.mp4', '.svg' ];
 
 async function getFromStorage(pathname, env) {
@@ -28,8 +30,14 @@ export default {
 
     if (!org || !site) return get404();
 
-    if (isEmbeddableAsset(pathname) || env.ADMIN_EXCEPTED_ORGS?.split(',').includes(org)) {
+    if (isEmbeddableAsset(pathname)) {
       return await getFromStorage(pathname, env);
+    }
+
+    if (env.ADMIN_EXCEPTED_ORGS?.split(',').includes(org)
+      && !req.headers.get('Authorization')
+      && req.headers.get('cf-connecting-ip') === HELIX_ADMIN_IP) {
+        return await getFromStorage(pathname, env);
     }
 
     return await getFromAdmin(req, env);
